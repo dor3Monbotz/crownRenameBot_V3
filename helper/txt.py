3bb73bc7bfc79361f793aceb53f9a6639b7a5847
dor3Monbotz <118621248+dor3Monbotz@users.noreply.github.com>
2022-12-09 03:19:56 +0530

    Update txt.py

--- helper/txt.py
+++ helper/txt.py
@@ -34,16 +34,25 @@ class mr(object):
 ╰━━━━━━━━━━━━━━━➣ """
 
     ABOUT_TXT = """
-╭───────────⍟
-├🤖 𝙼𝚈 𝙽𝙰𝙼𝙴 : {}
-├👑 𝙳𝙴𝚅𝙴𝙻𝙾𝙿𝙴𝚁𝚂 : <a href=https://t.me/projectcrown>𝚃𝚎𝚊𝚖 𝙲𝚛𝚘𝚠𝚗 𝙱𝚘𝚝𝚣</a> 
-├👨‍💻 𝙿𝚁𝙾𝙶𝚁𝙰𝙼𝙴𝚁 : <a href=https://t.me/little_little_hackur>𝙱𝚕𝚊𝚌𝚔 𝙷𝚊𝚝</a>
-├📕 𝙻𝙸𝙱𝚁𝙰𝚁𝚈 : <a href=https://github.com/pyrogram>𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼</a>
-├✏️ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴 : <a href=https://www.python.org>𝙿𝚈𝚃𝙷𝙾𝙽 3</a>
-├💾 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴 : <a href=https://cloud.mongodb.com>𝙼𝙾𝙽𝙶𝙾𝙳𝙱</a>
-├🌀 𝙼𝚈 𝚂𝙴𝚁𝚅𝙴𝚁 : <a href=https://mogenius.com/home>𝙼𝙾𝙶𝙴𝙽𝙸𝚄𝚂</a>
-├📊 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚄𝚂 : v3.6.8 [ 𝙼𝙰𝙹𝙾𝚁 ]              
-╰───────────────⍟
+╔════❰ CROWN RENAME BOT ❱═❍⊱❁۪۪
+
+║╭━━━━━━━━━━━━━━━➣
+
+║┣⪼👑 𝙳𝙴𝚅𝙴𝙻𝙾𝙿𝙴𝚁𝚂 : <a href=https://t.me/projectcrown>𝚃𝚎𝚊𝚖 𝙲𝚛𝚘𝚠𝚗 𝙱𝚘𝚝𝚣</a> 
+
+║┣⪼👨‍💻 𝙿𝚁𝙾𝙶𝚁𝙰𝙼𝙴𝚁 : <a href=https://t.me/little_little_hackur>𝙱𝚕𝚊𝚌𝚔 𝙷𝚊𝚝</a>
+
+║┣⪼✏️ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴 : <a href=https://www.python.org>𝙿𝚈𝚃𝙷𝙾𝙽 3</a>
+
+║┣⪼🌀 𝙼𝚈 𝚂𝙴𝚁𝚅𝙴𝚁 : <a href=https://mogenius.com/home>𝙼𝙾𝙶𝙴𝙽𝙸𝚄𝚂</a>
+
+║┣⪼📕 𝙻𝙸𝙱𝚁𝙰𝚁𝚈 : <a href=https://github.com/pyrogram>𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼</a>
+
+║┣⪼📊 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚄𝚂 : v3.6.8 [ 𝙼𝙰𝙹𝙾𝚁 ]
+
+║╰━━━━━━━━━━━━━━━➣
+
+╚══════════════════❍⊱❁۪۪
                                 """
     HELP_TXT = """
 🌌 <b><u>HOW TO SET THUMBNILE</u></b>
